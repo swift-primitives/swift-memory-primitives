53119e8c4c1f2dbb1816e3fa25c62e447d13f548
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-26 13:01:47 +0100

    Add throwing initializers for optional pointers to Memory.Address
    - Add Memory.Address.Error with null case
    - Add throwing init(_ pointer: UnsafeRawPointer?) throws(Error)
    - Add throwing init<T>(_ pointer: UnsafePointer<T>?) throws(Error)
    - Add throwing init<T>(_ pointer: UnsafeMutablePointer<T>?) throws(Error)
    
    Enables clean syntax: try Pointer<Int>(buffer.baseAddress)
    instead of force unwrap: Pointer<Int>(buffer.baseAddress!)

--- Sources/Memory Primitives/Memory.Address.swift
+++ Sources/Memory Primitives/Memory.Address.swift
@@ -23,6 +23,8 @@ extension Memory {
         @usableFromInline
         internal let _rawPointer: UnsafeRawPointer
 
+        // MARK: - Non-Optional Initializers
+
         /// Creates an address from a raw pointer.
         ///
         /// - Parameter pointer: A non-null raw pointer.
@@ -43,10 +45,54 @@ extension Memory {
             unsafe self._rawPointer = UnsafeRawPointer(pointer)
         }
 
+        // MARK: - Optional Initializers (Throwing)
+
+        /// Creates an address from an optional raw pointer.
+        ///
+        /// - Parameter pointer: An optional raw pointer.
+        /// - Throws: `Error.null` if the pointer is nil.
+        @inlinable
+        public init(_ pointer: UnsafeRawPointer?) throws(Error) {
+            guard let pointer else { throw .null }
+            unsafe self._rawPointer = unsafe pointer
+        }
+
+        /// Creates an address from an optional typed pointer.
+        ///
+        /// - Parameter pointer: An optional typed pointer.
+        /// - Throws: `Error.null` if the pointer is nil.
+        @inlinable
+        public init<T>(_ pointer: UnsafePointer<T>?) throws(Error) {
+            guard let pointer else { throw .null }
+            unsafe self._rawPointer = UnsafeRawPointer(pointer)
+        }
+
+        /// Creates an address from an optional mutable typed pointer.
+        ///
+        /// - Parameter pointer: An optional mutable typed pointer.
+        /// - Throws: `Error.null` if the pointer is nil.
+        @inlinable
+        public init<T>(_ pointer: UnsafeMutablePointer<T>?) throws(Error) {
+            guard let pointer else { throw .null }
+            unsafe self._rawPointer = UnsafeRawPointer(pointer)
+        }
+
+        // MARK: - Properties
+
         /// The raw pointer value.
         @inlinable
         public var rawPointer: UnsafeRawPointer {
             unsafe _rawPointer
         }
+    }
+}
+
+// MARK: - Error
+
+extension Memory.Address {
+    /// Errors that can occur when creating a memory address.
+    public enum Error: Swift.Error, Equatable, Hashable, Sendable {
+        /// The pointer was null.
+        case null
     }
 }
